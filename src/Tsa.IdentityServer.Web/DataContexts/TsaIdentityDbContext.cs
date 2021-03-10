e5bd60e502cd9b1faf93a7fdb55ec879b7a5fb1a
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2021-03-10 13:54:51 -0500

    Created context manage identities

---
+++ src/Tsa.IdentityServer.Web/DataContexts/TsaIdentityDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tsa.IdentityServer.Web.DataContexts
+{
+    public class TsaIdentityDbContext : IdentityDbContext
+    {
+        public TsaIdentityDbContext(DbContextOptions<TsaIdentityDbContext> options) : base(options) { }
+    }
+}
