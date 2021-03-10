ac7b2aedac1850af0365a8b3fdac14d228d3b2b9
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2021-03-10 13:54:07 -0500

    Created enum that maps to database seeding

---
+++ src/Tsa.IdentityServer.Web/Configuration/DatabaseSeedSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tsa.IdentityServer.Web.Configuration
+{
+    public enum DatabaseSeedSource
+    {
+        None,
+        Local,
+        LocalStorage,
+        AzureStorage
+    }
+}
