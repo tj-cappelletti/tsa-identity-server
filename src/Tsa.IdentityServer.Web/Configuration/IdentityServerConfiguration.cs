8e25eb23c90595967d554bf62d274d4754f06d00
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2021-03-11 20:23:39 -0500

    Created ability to seed database at startup
    Added ability to seed database from project for local debugging only
    Added ability to seed database from a file on the host system
    The intent is to mount a volume at run time with the seed data
    Stubed out for seeding from Azure storage, still needs to be implemented

---
+++ src/Tsa.IdentityServer.Web/Configuration/IdentityServerConfiguration.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Tsa.IdentityServer.Web.DataContexts;
+
+namespace Tsa.IdentityServer.Web.Configuration
+{
+    public sealed class IdentityServerConfiguration : IDisposable
+    {
+        private readonly ConfigurationDbContext _configurationDbContext;
+        private readonly string _configurationLocation;
+        private readonly ConfigurationSource _configurationSource;
+        private readonly ILogger _logger;
+        private readonly PersistedGrantDbContext _persistedGrantDbContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly TsaIdentityDbContext _tsaIdentityDbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityServerConfiguration(IApplicationBuilder applicationBuilder, ConfigurationSource configurationSource, string configurationLocation)
+        {
+            _configurationLocation = configurationLocation;
+            _configurationSource = configurationSource;
+
+            // ReSharper disable once PossibleNullReferenceException
+            // If the IServiceScopeFactory is null here, let the
+            // exception bubble up as this method isn't being called
+            // at the right time
+            var serviceScope = applicationBuilder.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
+
+            _configurationDbContext = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            _persistedGrantDbContext = serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>();
+            _roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            _tsaIdentityDbContext = serviceScope.ServiceProvider.GetRequiredService<TsaIdentityDbContext>();
+            _userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var loggerFactory = applicationBuilder.ApplicationServices.GetService<ILoggerFactory>();
+            // ReSharper disable once PossibleNullReferenceException
+            // If loggerFactory is null, this was called at the wrong time
+            // hence, let it die with a NullReferenceException
+            _logger = loggerFactory.CreateLogger("Tsa.IdentityServer.Web.IdentityServerConfiguration");
+        }
+
+        private void AddIdentityServerApiResources(IEnumerable<ApiResource> apiResources)
+        {
+            foreach (var apiResource in apiResources)
+            {
+                if (_configurationDbContext.ApiResources.Any(ar=>ar.Name == apiResource.Name)) continue;
+
+                _logger.LogInformation("Creating IdentityServer API Resource {apiResource}", apiResource.Name);
+
+                _configurationDbContext.ApiResources.Add(apiResource.ToEntity());
+            }
+        }
+
+        private void AddIdentityServerApiScopes(IEnumerable<ApiScope> apiScopes)
+        {
+            foreach (var apiScope in apiScopes)
+            {
+                if (_configurationDbContext.ApiScopes.Any(a => a.Name == apiScope.Name)) continue;
+
+                _logger.LogInformation("Creating IdentityServer API Scope {apiScopes}", apiScope.Name);
+
+                _configurationDbContext.ApiScopes.Add(apiScope.ToEntity());
+            }
+        }
+
+        private void AddIdentityServerClients(IEnumerable<Client> clients)
+        {
+            foreach (var client in clients)
+            {
+                if (_configurationDbContext.Clients.Any(c => c.ClientName == client.ClientName)) continue;
+
+                var secrets = client.ClientSecrets.Select(s => s.Description).ToList();
+
+                client.ClientSecrets.Clear();
+
+                foreach (var secret in secrets) client.ClientSecrets.Add(new Secret(secret.Sha256()));
+
+                _logger.LogInformation("Creating IdentityServer Client {client}", client.ClientName);
+
+                _configurationDbContext.Clients.Add(client.ToEntity());
+            }
+
+            _configurationDbContext.SaveChanges();
+        }
+
+        private void AddIdentityServerRoles(IEnumerable<string> roles)
+        {
+            foreach (var role in roles)
+            {
+                var identityRole = _roleManager.FindByNameAsync(role).Result;
+
+                if (identityRole != null) continue;
+
+                _logger.LogInformation("Creating the role {role}", role);
+
+                var identityResult = _roleManager.CreateAsync(new IdentityRole(role)).Result;
+
+                if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
+            }
+        }
+
+        private void AddIdentityServerUserRoles(IEnumerable<IdentityUserRole> identityUserRoles)
+        {
+            foreach (var identityUserRole in identityUserRoles)
+            {
+                var identityUsers = _userManager.GetUsersInRoleAsync(identityUserRole.RoleName).Result;
+
+                if (identityUsers.Any(iu => iu.Id == identityUserRole.UserId)) continue;
+
+                var identityUser = _userManager.FindByIdAsync(identityUserRole.UserId).Result;
+
+                if (identityUser == null) throw new NullReferenceException("Could not find user while assigning them to the role.");
+
+                _logger.LogInformation("Assigning the user {identityUser} to the role {role}", identityUser.Id, identityUserRole.RoleName);
+
+                var identityResult = _userManager.AddToRoleAsync(identityUser, identityUserRole.RoleName).Result;
+
+                if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
+            }
+        }
+
+        private void AddIdentityServerUsers(IEnumerable<IdentityUser> identityUsers)
+        {
+            foreach (var identityUser in identityUsers)
+            {
+                if (_userManager.FindByIdAsync(identityUser.Id).Result != null) continue;
+
+                var password = identityUser.PasswordHash;
+                identityUser.PasswordHash = string.Empty;
+
+                _logger.LogInformation("Creating the user {identityUser}", identityUser.Id);
+
+                var identityResult = _userManager.CreateAsync(identityUser, password).Result;
+
+                if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
+            }
+        }
+
+        public void ApplyDatabaseMigrations()
+        {
+            _logger.LogInformation("Running database migration for the {dbContext}", "PersistedGrantDbContext");
+            _persistedGrantDbContext.Database.Migrate();
+
+            _logger.LogInformation("Running database migration for the {dbContext}", "ConfigurationDbContext");
+            _configurationDbContext.Database.Migrate();
+
+            _logger.LogInformation("Running database migration for the {dbContext}", "TsaIdentityDbContext");
+            _tsaIdentityDbContext.Database.Migrate();
+        }
+
+        public void Dispose()
+        {
+            _configurationDbContext.Dispose();
+            _persistedGrantDbContext.Dispose();
+            _roleManager.Dispose();
+            _tsaIdentityDbContext.Dispose();
+            _userManager.Dispose();
+        }
+
+        public void LoadConfiguration()
+        {
+            _logger.LogInformation("Loading configuration from: {configSource}", _configurationSource);
+            string identityServerSeedDataJson;
+
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+            // There is nothing to do when the source is none
+            // In a default case, do nothing and assume it is a second instance
+            // that needs to connect to an existing database
+            switch (_configurationSource)
+            {
+#if DEBUG
+                // Project should only be available when the DEBUG flag is set
+                // This is to help safeguard production from having testing
+                // settings and credentials published
+                case ConfigurationSource.Project:
+                    identityServerSeedDataJson = File.ReadAllText("identityserverseeddata.json");
+                    break;
+#endif
+                case ConfigurationSource.SystemStorage:
+                    identityServerSeedDataJson = File.ReadAllText(_configurationLocation);
+                    break;
+
+                case ConfigurationSource.AzureStorage:
+                    throw new NotImplementedException();
+
+                default:
+                    return;
+            }
+
+            if (string.IsNullOrWhiteSpace(identityServerSeedDataJson)) return;
+
+            var identityServerSeedData = JsonSerializer.Deserialize<IdentityServerSeedData>(identityServerSeedDataJson);
+
+            if (identityServerSeedData == null) throw new NullReferenceException("Unable to deserialize the seed data file.");
+
+            AddIdentityServerApiResources(identityServerSeedData.ApiResources);
+            AddIdentityServerApiScopes(identityServerSeedData.ApiScopes);
+            AddIdentityServerClients(identityServerSeedData.Clients);
+            AddIdentityServerRoles(identityServerSeedData.Roles);
+            AddIdentityServerUsers(identityServerSeedData.IdentityUsers);
+            AddIdentityServerUserRoles(identityServerSeedData.IdentityUserRoles);
+        }
+
+        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+        private class IdentityServerSeedData
+        {
+            public IEnumerable<ApiResource> ApiResources { get; set; }
+
+            public IEnumerable<ApiScope> ApiScopes { get; set; }
+
+            public IEnumerable<Client> Clients { get; set; }
+
+            public IEnumerable<IdentityUserRole> IdentityUserRoles { get; set; }
+
+            public IEnumerable<IdentityUser> IdentityUsers { get; set; }
+
+            public IEnumerable<string> Roles { get; set; }
+        }
+
+        [SuppressMessage("ReSharper", "ClassNeverInstantiated.Local")]
+        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+        private class IdentityUserRole
+        {
+            public string RoleName { get; set; }
+
+            public string UserId { get; set; }
+        }
+    }
+}
--- src/Tsa.IdentityServer.Web/Startup.cs
+++ src/Tsa.IdentityServer.Web/Startup.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using IdentityServer4;
-using IdentityServer4.EntityFramework.DbContexts;
-using IdentityServer4.EntityFramework.Mappers;
-using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -13,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Tsa.IdentityServer.Web.Configuration;
 using Tsa.IdentityServer.Web.DataContexts;
 
@@ -23,6 +15,7 @@ namespace Tsa.IdentityServer.Web
     {
         private const string CreateDatabaseArgumentKey = "createDb";
         private const string SeedDatabaseSourceKey = "dbSeedSource";
+        private const string SeedDatabaseLocationKey = "dbSeedLocation";
 
         public IConfiguration Configuration { get; set; }
 
@@ -32,177 +25,6 @@ namespace Tsa.IdentityServer.Web
         {
             Configuration = configuration;
             WebHostEnvironment = webHostEnvironment;
-        }
-
-        private static void AddLocalIdentityServerApiResources(ConfigurationDbContext configurationDbContext, ILogger logger)
-        {
-            var apiResources = new List<ApiResource>
-            {
-                new("tsa.submissions", "TSA Submissions API")
-                {
-                    Scopes = { "tsa.submissions.read", "tsa.submissions.create" }
-                },
-                new("tsa.coding.submissions", "TSA Coding Submissions API")
-                {
-                    Scopes = { "tsa.coding.submissions.read", "tsa.coding.submissions.create" }
-                }
-            };
-
-            foreach (var apiResource in apiResources.Where(apiScope => !configurationDbContext.ApiResources.Any(a => a.Name == apiScope.Name)))
-            {
-                logger.LogInformation("Creating the API resource {apiResource}", apiResource.Name);
-                configurationDbContext.ApiResources.Add(apiResource.ToEntity());
-            }
-        }
-
-        private static void AddLocalIdentityServerApiScopes(ConfigurationDbContext configurationDbContext, ILogger logger)
-        {
-            var apiScopes = new List<ApiScope>
-            {
-                new("tsa.submissions.read", "Display/read submissions"),
-                new("tsa.submissions.create", "Create submissions"),
-                new("tsa.coding.submissions.read", "Display/read coding submissions"),
-                new("tsa.coding.submissions.create", "Create coding submissions")
-            };
-
-            foreach (var apiScope in apiScopes.Where(apiScope => !configurationDbContext.ApiScopes.Any(a => a.Name == apiScope.Name)))
-            {
-                logger.LogInformation("Creating the API scope {apiScope}", apiScope.Name);
-                configurationDbContext.ApiScopes.Add(apiScope.ToEntity());
-            }
-        }
-
-        private static void AddLocalIdentityServerClients(ConfigurationDbContext configurationDbContext, ILogger logger)
-        {
-            var clients = new List<Client>
-            {
-                new()
-                {
-                    AllowOfflineAccess = false,
-                    AllowPlainTextPkce = false,
-                    AllowedGrantTypes = GrantTypes.Code,
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "tsa.coding.submissions.read",
-                        "tsa.coding.submissions.create"
-                    },
-                    ClientId = "tsa.coding.submissions.web",
-                    ClientName = "TSA Coding Submissions Web UI",
-                    ClientSecrets = { new Secret("a673bbae-71e4-4962-a623-665689c4dd34".Sha256()) },
-                    PostLogoutRedirectUris = { "https://localhost:44345/signout-oidc" },
-                    RedirectUris = { "https://localhost:44345/signin-oidc" },
-                    RequireConsent = false,
-                    RequirePkce = true
-                },
-                new()
-                {
-                    AllowOfflineAccess = false,
-                    AllowPlainTextPkce = false,
-                    AllowedGrantTypes = GrantTypes.Code,
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "submissions.read",
-                        "submissions.create"
-                    },
-                    ClientId = "tsa.submissions.web",
-                    ClientName = "TSA Submissions Web UI",
-                    ClientSecrets = { new Secret("945931d5-6100-4129-b2c0-d9e9b34d1828".Sha256()) },
-                    RequireConsent = false,
-                    RequirePkce = true
-                }
-            };
-
-            foreach (var client in clients.Where(apiScope => !configurationDbContext.Clients.Any(c => c.ClientId == apiScope.ClientId)))
-            {
-                logger.LogInformation("Creating the client {client}", client.ClientId);
-                configurationDbContext.Clients.Add(client.ToEntity());
-            }
-        }
-
-        private static void AddLocalIdentityServerConfiguration(IApplicationBuilder app)
-        {
-            // ReSharper disable once PossibleNullReferenceException
-            // If the IServiceScopeFactory is null here, let the
-            // exception bubble up as this method isn't being called
-            // at the right time
-            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
-            var configurationDbContext = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-
-            var loggerFactory = app.ApplicationServices.GetService<ILoggerFactory>();
-            
-            if (loggerFactory == null) throw new NullReferenceException($"The `{nameof(loggerFactory)}` was null");
-
-            var logger = loggerFactory.CreateLogger("Tsa.IdentityServer.Web.Startup");
-
-            //TODO: Move to a configuration file
-            AddLocalIdentityServerApiScopes(configurationDbContext, logger);
-            AddLocalIdentityServerApiResources(configurationDbContext, logger);
-            AddLocalIdentityServerClients(configurationDbContext, logger);
-
-            configurationDbContext.SaveChanges();
-        }
-
-        public static void AddLocalIdentityServerUsers(IApplicationBuilder app)
-        {
-            // ReSharper disable once PossibleNullReferenceException
-            // If the IServiceScopeFactory is null here, let the
-            // exception bubble up as this method isn't being called
-            // at the right time
-            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
-
-            var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-            var loggerFactory = app.ApplicationServices.GetService<ILoggerFactory>();
-
-            if (loggerFactory == null) throw new NullReferenceException($"The `{nameof(loggerFactory)}` was null");
-
-            var logger = loggerFactory.CreateLogger("Tsa.IdentityServer.Web.Startup");
-
-            CreateLocalIdentityServerRole(roleManager, "judge", logger);
-            CreateLocalIdentityServerRole(roleManager, "participant", logger);
-
-            var studentOneIdentityUser = new IdentityUser
-            {
-                UserName = "9999-001",
-                Email = "student1@tsa.local",
-                EmailConfirmed = true,
-                Id = "9999-001"
-            };
-
-            var studentTwoIdentityUser = new IdentityUser
-            {
-                UserName = "9999-002",
-                Email = "student2@tsa.local",
-                EmailConfirmed = true,
-                Id = "9999-002"
-            };
-
-            CreateLocalIdentityServerUser(userManager, studentOneIdentityUser, "participant", logger);
-            CreateLocalIdentityServerUser(userManager, studentTwoIdentityUser, "participant", logger);
-
-            var judgeOneIdentityUser = new IdentityUser
-            {
-                UserName = "judge01",
-                Email = "judge01@tsa.local",
-                EmailConfirmed = true,
-                Id = "judge01"
-            };
-
-            var judgeTwoIdentityUser = new IdentityUser
-            {
-                UserName = "judge02",
-                Email = "judge02@tsa.local",
-                EmailConfirmed = true,
-                Id = "judge02"
-            };
-
-            CreateLocalIdentityServerUser(userManager, judgeOneIdentityUser, "judge", logger);
-            CreateLocalIdentityServerUser(userManager, judgeTwoIdentityUser, "judge", logger);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -255,91 +77,30 @@ namespace Tsa.IdentityServer.Web
             services.AddControllersWithViews();
         }
 
-        private static void CreateLocalIdentityServerRole(RoleManager<IdentityRole> roleManager, string role, ILogger logger)
-        {
-            if (roleManager.RoleExistsAsync(role).Result) return;
-
-            logger.LogInformation("Creating the role {role}", role);
-
-            var identityResult = roleManager.CreateAsync(new IdentityRole(role)).Result;
-
-            if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
-        }
-
-        private static void CreateLocalIdentityServerUser(UserManager<IdentityUser> userManager, IdentityUser identityUser, string role, ILogger logger)
-        {
-            if (userManager.FindByIdAsync(identityUser.Id).Result != null) return;
-
-            logger.LogInformation("Creating the user {identityUser}", identityUser.Id);
-
-            var identityResult = userManager.CreateAsync(identityUser, "Pa$$w0rd").Result;
-
-            if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
-
-            logger.LogInformation("Assigning the user {identityUser} to the role {role}", identityUser.Id, role);
-            identityResult = userManager.AddToRoleAsync(identityUser, role).Result;
-
-            if (!identityResult.Succeeded) throw new Exception(identityResult.Errors.First().Description);
-        }
-
         private void InitializeDatabase(IApplicationBuilder app)
         {
             var createDb = Configuration.GetValue<bool>(CreateDatabaseArgumentKey);
-            if (createDb) ApplyDatabaseMigrations(app);
-
             var databaseSeedSourceValue = Configuration[SeedDatabaseSourceKey];
-            DatabaseSeedSource databaseSeedSource;
+            var databaseSeedLocation = Configuration[SeedDatabaseLocationKey];
+
+            ConfigurationSource databaseSeedSource;
 
             if (string.IsNullOrWhiteSpace(databaseSeedSourceValue))
-                databaseSeedSource = DatabaseSeedSource.None;
+                databaseSeedSource = ConfigurationSource.None;
             else
                 databaseSeedSource = databaseSeedSourceValue.ToLower() switch
                 {
-                    "local" => DatabaseSeedSource.Local,
-                    _ => DatabaseSeedSource.None
+                    "project" => ConfigurationSource.Project,
+                    "system" => ConfigurationSource.SystemStorage,
+                    "azure" => ConfigurationSource.AzureStorage,
+                    _ => ConfigurationSource.None
                 };
 
-            // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
-            // There is nothing to do when the source is none
-            // In a default case, do nothing and assume it is a second instance
-            // that needs to connect to an existing database
-            switch (databaseSeedSource)
-            {
-                case DatabaseSeedSource.Local:
-                    AddLocalIdentityServerConfiguration(app);
-                    AddLocalIdentityServerUsers(app);
-                    break;
+            var identityServerConfiguration = new IdentityServerConfiguration(app, databaseSeedSource, databaseSeedLocation);
 
-                case DatabaseSeedSource.LocalStorage:
-                    throw new NotImplementedException();
+            if (createDb) identityServerConfiguration.ApplyDatabaseMigrations();
 
-                case DatabaseSeedSource.AzureStorage:
-                    throw new NotImplementedException();
-            }
-        }
-
-        private static void ApplyDatabaseMigrations(IApplicationBuilder app)
-        {
-            // ReSharper disable once PossibleNullReferenceException
-            // If the IServiceScopeFactory is null here, let the
-            // exception bubble up as this method isn't being called
-            // at the right time
-            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
-
-            var loggerFactory = app.ApplicationServices.GetService<ILoggerFactory>();
-
-            if (loggerFactory == null) throw new NullReferenceException($"The `{nameof(loggerFactory)}` was null");
-
-            var logger = loggerFactory.CreateLogger("Tsa.IdentityServer.Web.Startup");
-
-            logger.LogInformation("Running database migration for the {dbContext}", "PersistedGrantDbContext");
-            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
-
-            logger.LogInformation("Running database migration for the {dbContext}", "ConfigurationDbContext");
-            serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
-
-            logger.LogInformation("Running database migration for the {dbContext}", "TsaIdentityDbContext");
-            serviceScope.ServiceProvider.GetRequiredService<TsaIdentityDbContext>().Database.Migrate();
+            identityServerConfiguration.LoadConfiguration();
         }
     }
 }
